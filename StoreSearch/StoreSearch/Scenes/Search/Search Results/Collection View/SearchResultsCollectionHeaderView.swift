349320cf8f82ceb026727296ad7762acd581ab89
CypherPoet <CypherPoet@gmail.com>
2019-08-13 22:14:36 -0400

    First pass at localized quotation marks

--- StoreSearch/StoreSearch/Scenes/Search/Search Results/Collection View/SearchResultsCollectionHeaderView.swift
+++ StoreSearch/StoreSearch/Scenes/Search/Search Results/Collection View/SearchResultsCollectionHeaderView.swift
@@ -60,7 +60,10 @@ extension SearchResultsCollectionHeaderView {
         
         
         var headerText: String {
-            "\(beginningText) \"\(searchText ?? "")\""
+            let beginningQuoteMark = Locale.current.quotationBeginDelimiter ?? "\""
+            let endQuoteMark = Locale.current.quotationEndDelimiter ?? "\""
+            
+            return "\(beginningText) \(beginningQuoteMark)\(searchText ?? "")\(endQuoteMark)"
         }
     }
 }
